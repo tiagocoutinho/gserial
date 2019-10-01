cc65eab23ce22bafcad8260db99361925c7663e2
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-10-01 22:10:56 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Tiago Coutinho",
+    author_email='coutinhotiago@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="A gevent friendly serial line",
+    install_requires=['gevent', 'pyserial'],
+    license="GPL",
+    long_description="A gevent friendly serial line",
+    keywords='pyserial, gevent',
+    name='gserial',
+    packages=find_packages(include=['gserial']),
+    url='https://github.com/tiagocoutinho/gserial',
+    version='0.1.0')
