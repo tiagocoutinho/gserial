4450defee73942aca88be399ce8a6deb4aa22f77
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-12-23 15:44:56 +0100

    Better example

--- examples/scpi_serial_server.py
+++ examples/scpi_serial_server.py
@@ -15,17 +15,21 @@ os.symlink(port, name)
 def handle():
     while True:
         data = os.read(master, 1024)
-        datau = data.upper().strip()
+        datau = data.upper().strip().decode()
         print('processing {!r}'.format(data))
-        if datau == b'*IDN?':
+        if datau == '*IDN?':
             msg = 'RoadRunner, ACME Inc., v1013, 234567\n'
         else:
             msg = 'ERROR: Unknown command\n'
-        print('start reply with {!r}... '.format(msg), end='', flush=True)
+        print('start reply with: \'', end='', flush=True)
         for c in msg:
             os.write(master, c.encode())
-            time.sleep(0.05)
-        print('[DONE]')
+            if c in '\n\r\t':
+                c = repr(c)[1:-1]
+            print(c, end='', flush=True)
+            time.sleep(0.02)
+        print('\'... [DONE]')
+
 
 print('Ready to accept requests on {}'.format(name))
 try:
